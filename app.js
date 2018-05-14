0f8223b49850aabe086cf3f9210ebd87fa7a58a8
Cecily Grossmann <grssmnn2@gmail.com>
2018-05-14 17:25:06 -0500

    adds color change to pressed keys

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 // when specific key is pressed, play sound associated with key code
-window.addEventListener('keydown', function(e){
-    const audio = document.querySelector()
-    if(!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
+window.addEventListener('keydown', function () {
+
+    if (event.keyCode === 65) {
+        document.getElementById("a").style.color = "red";
+        document.addEventListener('keyup', function () {
+            document.getElementById("a").style.color = "black";
+        })
+    }else if (event.keyCode === 83) {
+        document.getElementById("s").style.color = "red";
+        document.addEventListener('keyup', function () {
+            document.getElementById("s").style.color = "black";
+        })
+    }else if (event.keyCode === 68) {
+        document.getElementById("d").style.color = "red";
+        document.addEventListener('keyup', function () {
+            document.getElementById("d").style.color = "black";
+        })
+    }else if (event.keyCode === 70) {
+        document.getElementById("f").style.color = "red";
+        document.addEventListener('keyup', function () {
+            document.getElementById("f").style.color = "black";
+        })
+    }else if (event.keyCode === 74) {
+        document.getElementById("j").style.color = "red";
+        document.addEventListener('keyup', function () {
+            document.getElementById("j").style.color = "black";
+        })
+    }else if (event.keyCode === 75) {
+        document.getElementById("k").style.color = "red";
+        document.addEventListener('keyup', function () {
+            document.getElementById("k").style.color = "black";
+        })
+    }else if (event.keyCode === 76) {
+        document.getElementById("l").style.color = "red";
+        document.addEventListener('keyup', function () {
+            document.getElementById("l").style.color = "black";
+        })
+    }
 });
+
